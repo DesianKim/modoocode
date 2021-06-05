adfab231e026a22053fe9605e594ab1ea763556d
DESKTOP-GJ1F6V0\taeyo <taeyoungkimtv@gmail.com>
2021-06-05 16:08:23 +0900

    Stack class 구현

---
+++ src/modoocode/MyStack.cpp
@@ -0,0 +1,59 @@
+#include "pch.h"
+#include "MyStack.h"
+
+Excel::Stack::Stack()
+	: m_start(nullptr, "")
+{
+	m_pCurrent = &m_start;
+}
+
+Excel::Stack::~Stack()
+{
+	while (m_pCurrent != &m_start)
+	{
+		Node* temp = m_pCurrent->pPrev;
+		m_pCurrent = temp->pPrev;
+
+		delete temp;
+	}
+	delete m_pCurrent;
+}
+
+void Excel::Stack::push(const DataType& Data) noexcept
+{
+	Node* temp = new Node(m_pCurrent, Data);
+	m_pCurrent = temp;
+}
+
+const Excel::DataType Excel::Stack::pop()
+{
+	if (m_pCurrent == &m_start)
+		return m_start.nodeData;
+
+	DataType tempData = m_pCurrent->nodeData;
+	Node* pTemp = m_pCurrent;
+
+	m_pCurrent = pTemp->pPrev;
+	delete pTemp;
+
+	return tempData;
+}
+
+const Excel::DataType& Excel::Stack::peek() const
+{
+	return m_pCurrent->nodeData;
+}
+
+bool Excel::Stack::isEmpty() const
+{
+	if (m_pCurrent == &m_start)
+		return true;
+	else
+		return false;
+}
+
+Excel::Stack::Node::Node(Node* prev, DataType Data) noexcept
+	: pPrev(prev),
+	nodeData(Data)
+{
+}
---
+++ src/modoocode/MyStack.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+
+namespace Excel
+{
+	using DataType = std::string;
+	class Stack
+	{
+	public:
+		Stack();
+		virtual ~Stack();
+
+		void push(const DataType& Data) noexcept;
+		const DataType pop();
+		const DataType& peek() const;
+		bool isEmpty() const;
+
+	private:
+		struct Node
+		{
+			Node* pPrev;
+			DataType nodeData;
+
+			Node(Node* prev, DataType Data) noexcept;
+		}; // struct Node
+
+		Node* m_pCurrent;
+		Node m_start;
+	};// class Stack
+}// namespace Excel
+
