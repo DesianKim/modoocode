b21435ad4beb78129378d06b6c8800ae7f17b1a5
teayoungkim <aslkdj1@nextchip.com>
2021-06-29 09:14:21 +0900

    + add default tempate argument

--- src/modoocode/FuncTemplate.h
+++ src/modoocode/FuncTemplate.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 
 struct Comp1
 {
@@ -11,6 +12,15 @@ struct Comp1
 struct Comp2
 {
 	bool operator()(int32_t a, int32_t b)
+	{
+		return a < b;
+	}
+};
+
+template <typename T>
+struct Compare
+{
+	bool operator()(const T&a, const T&b) const
 	{
 		return a < b;
 	}
@@ -38,4 +48,27 @@ void bubble_sort(Cont& cont, Comp& comp)
 		for (int32_t j = i + 1; j < cont.size(); ++j)
 			if (!comp(cont[i], cont[j]))
 				cont.swap(i, j);
+}
+
+template <typename T, int num = 5>
+T add_num(T t)
+{
+	return t + num;
+}
+
+template <typename T>
+void print_array(const T& arr)
+{
+	for (int32_t i = 0; i < arr.size(); ++i)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
+
+template <typename T, typename Comp = Compare<T>>
+T Min(T a, T b)
+{
+	Comp comp;
+	if (comp(a, b))
+		return a;
+	return b;
 }
