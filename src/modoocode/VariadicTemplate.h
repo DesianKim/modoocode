ca347d48b8b1c24767a8af4c68a1777b3ebbf045
teayoungkim <aslkdj1@nextchip.com>
2021-06-30 08:46:48 +0900

    + add StrCat Variadic function

--- src/modoocode/VariadicTemplate.h
+++ src/modoocode/VariadicTemplate.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <string>
 
 template <typename T>
 void print(T arg)
@@ -12,4 +13,47 @@ void print(T arg, Types... args)
 {
 	std::cout << arg << ", ";
 	print(args...);
+}
+
+size_t GetStringSize(const char* s)
+{
+	return strlen(s);
+}
+
+size_t GetStringSize(const std::string& s)
+{
+	return s.size();
+}
+
+template <typename T, typename... Types>
+size_t GetStringSize(const T& s, Types... strs)
+{
+	return GetStringSize(s) + GetStringSize(strs...);
+}
+
+template <typename T, typename... Types>
+std::string StrCat(const T& s, Types... strs)
+{
+	size_t total_size = GetStringSize(s, strs...);
+
+	std::string concat_str;
+	concat_str.reserve(total_size);
+
+	concat_str = s;
+
+	AppendToString(&concat_str, strs...);
+
+	return concat_str;
+}
+
+void AppendToString(std::string* concat_str)
+{
+	return;
+}
+
+template <typename T, typename... Types>
+void AppendToString(std::string* concat_str, const T& s, Types... strs)
+{
+	concat_str->append(s);
+	AppendToString(concat_str, strs...);
 }
