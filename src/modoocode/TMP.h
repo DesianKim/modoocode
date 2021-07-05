44e82675a7680927896f9051f5bf84f5cd53c5b3
teayoungkim <aslkdj1@nextchip.com>
2021-07-06 08:31:39 +0900

    + add is_prime (TMP)

--- src/modoocode/TMP.h
+++ src/modoocode/TMP.h
@@ -36,3 +36,67 @@ struct GCD<X, 0>
 {
 	static const int32_t value = X;
 };
+
+
+template <uint32_t N>
+struct fib
+{
+	static const uint32_t result = fib<N - 1>::result + fib<N - 2>::result;
+};
+
+template<>
+struct fib<1>
+{
+	static const uint32_t result = 1;
+};
+
+template<>
+struct fib<2>
+{
+	static const uint32_t result = 1;
+};
+
+bool is_prime(int32_t N)
+{
+	if (N == 2)
+		return true;
+	if (N == 3)
+		return true;
+
+	for (int32_t i = 2; i <= N / 2; ++i)
+	{
+		if (N % i == 0)
+			return false;
+	}
+	return true;
+}
+
+template <>
+struct is_prime<2>
+{
+	static const bool result = true;
+};
+
+template <>
+struct is_prime<3>
+{
+	static const bool result = true;
+};
+
+template <int32_t N>
+struct is_prime
+{
+	static const bool result = !check_div<N, 2>::result;
+};
+
+template <int32_t N, int32_t d>
+struct check_div
+{
+	static const bool result = (N % d == 0) || check_div<N, d + 1>::result;
+};
+
+template <int32_t N>
+struct check_div<N, N/2>
+{
+	static const bool result = (N % (N / 2) == 0);
+};
