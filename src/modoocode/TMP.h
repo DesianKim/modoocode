fb7cf5d82248eecaa3a36afe57fe708150db68a8
teayoungkim <aslkdj1@nextchip.com>
2021-07-07 09:07:23 +0900

    + modify is_prime (TMP)

--- src/modoocode/TMP.h
+++ src/modoocode/TMP.h
@@ -1,17 +1,17 @@
 #pragma once
 #include <iostream>
 
-template <int32_t N>
-struct Int
-{
-	static const int32_t num = N;
-};
-
-template <typename T, typename U>
-struct add
-{
-	typedef Int<T::num + U::num> result;
-};
+//template <int32_t N>
+//struct Int
+//{
+//	static const int32_t num = N;
+//};
+//
+//template <typename T, typename U>
+//struct add
+//{
+//	typedef Int<T::num + U::num> result;
+//};
 
 template <int32_t N>
 struct Factorial
@@ -56,47 +56,76 @@ struct fib<2>
 	static const uint32_t result = 1;
 };
 
-bool is_prime(int32_t N)
-{
-	if (N == 2)
-		return true;
-	if (N == 3)
-		return true;
+//bool is_prime(int32_t N)
+//{
+//	if (N == 2)
+//		return true;
+//	if (N == 3)
+//		return true;
+//
+//	for (int32_t i = 2; i <= N / 2; ++i)
+//	{
+//		if (N % i == 0)
+//			return false;
+//	}
+//	return true;
+//}
 
-	for (int32_t i = 2; i <= N / 2; ++i)
-	{
-		if (N % i == 0)
-			return false;
-	}
-	return true;
-}
+template <int N>
+struct INT 
+{
+	static const int num = N;
+};
+
+template <typename a, typename b>
+struct add 
+{
+	using result = INT<a::num + b::num>;
+};
+
+template <typename a, typename b>
+struct divide 
+{
+	using result = INT<a::num / b::num>;
+};
+
+using one = INT<1>;
+using two = INT<2>;
+using three = INT<3>;
+
+template <typename N, typename d>
+struct check_div 
+{
+	static const bool result = (N::num % d::num == 0) ||
+		check_div<N, typename add<d, one>::result>::result;
+};
+
+template <typename N>
+struct _is_prime 
+{
+	static const bool result = !check_div<N, two>::result;
+};
 
 template <>
-struct is_prime<2>
+struct _is_prime<two> 
 {
 	static const bool result = true;
 };
 
 template <>
-struct is_prime<3>
+struct _is_prime<three> 
 {
 	static const bool result = true;
 };
 
-template <int32_t N>
-struct is_prime
+template <typename N>
+struct check_div<N, typename divide<N, two>::result> 
 {
-	static const bool result = !check_div<N, 2>::result;
+	static const bool result = (N::num % (N::num / 2) == 0);
 };
 
-template <int32_t N, int32_t d>
-struct check_div
+template <int N>
+struct is_prime 
 {
-	static const bool result = (N % d == 0) || check_div<N, d + 1>::result;
-};
-
-template <int32_t N>
-struct check_div<N, N/2>
-{
-	static const bool result = (N % (N / 2) == 0);
+	static const bool result = _is_prime<INT<N>>::result;
 };
