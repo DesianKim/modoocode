5150b5232c9affe3fcf572e2823f8bd86c182cc4
teayoungkim <aslkdj1@nextchip.com>
2021-07-19 10:02:23 +0900

    + add string_view (STL)

--- src/modoocode/MyString.h
+++ src/modoocode/MyString.h
@@ -31,9 +31,3 @@ struct my_char_traits : public std::char_traits<char>
 		return 0;
 	}
 };
-
-void* operator new(std::size_t count)
-{
-	std::cout << count << " bytes วาด็ " << std::endl;
-	return malloc(count);
-}
